b1219cc7ce8c24876757232a7d71441c9bcf1aeb
Joaquín Colonnello <joaquincolonnello@hotmail.com>
2021-03-10 17:41:20 -0300

    Implemented BFS
    Added stopwatch
    Added more examples

---
+++ TP1/Models/BFS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1.Models
+{
+    public class BFS : SearchTree
+    {
+        public BFS(State root) : base(root) { }
+        public override Node GetSolution()
+        {
+            var queue = new Queue<(Node node, int depth)>();
+            var explored = new HashSet<State>();
+            int currentDepth = 0;
+
+            queue.Enqueue((Root, 0));
+            while(queue.Count > 0)
+            {
+                (Node current, int depth) = queue.Dequeue();
+                explored.Add(current.State);
+                if(depth > currentDepth)
+                {
+                    currentDepth = depth;
+                    Console.WriteLine($"Current depth: {currentDepth}");
+                }
+                if (current.State.IsGoal)
+                    return current;
+
+                foreach(var action in current.State.PosibleActions())
+                {
+                    Node child = new Node(current, action.Value, action.Key);
+                    if (!explored.Contains(child.State))
+                        queue.Enqueue((child, depth+1));
+                }
+            }
+            return null;
+        }
+    }
+}
--- TP1/Program.cs
+++ TP1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using TP1.Models;
 using TP1.Sokoban;
 
@@ -9,9 +10,12 @@ namespace TP1
     {
         static void Main(string[] args)
         {
-            var game = SokobanFactory.FromFile("Maps/example3.txt");
-            var search = new IDDFS(10,game);
+            var game = SokobanFactory.FromFile("Maps/8x8.txt");
+            var search = new BFS(game);
+            var sw = new Stopwatch();
+            sw.Start();
             var solution = search.GetSolution();
+            sw.Stop();
 
             List<SokobanActions> actions = new List<SokobanActions>();
             while(solution.Parent != null)
@@ -20,8 +24,12 @@ namespace TP1
                 solution = solution.Parent;
             }
             actions.Reverse();
+            Console.WriteLine($"Solved in: {sw.ElapsedMilliseconds}ms");
             foreach (var action in actions)
-                Console.WriteLine(action);
+            {
+                Console.Write(action);
+                Console.Write('\t');
+            }
         }
     }
 }
